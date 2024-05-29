80f8359916f22e29db25e335a2ac208c205cf7e2
JustinLungu <iustinl113@gmail.com>
2024-05-29 15:08:57 +0200

    decide if preprocessing

--- Projects/main.py
+++ Projects/main.py
@@ -7,6 +7,8 @@ OVERLAP = 0.5 #0.5 = 50%
 VALID_ANNOTATIONS = {'N', 'R'}
 INVALID_ANNOTATIONS = {'~'}
 
+DO_PREPROCESSING = False
+
 if __name__ == "__main__":
     p_212 = Visualization(212)
     #p_212.multi_plot_label()
@@ -14,18 +16,20 @@ if __name__ == "__main__":
     #p_212.plot_annotation_signals()
 
     # Example usage
-    prep = Preprocessing(p_212.record, p_212.annotation, VALID_ANNOTATIONS, INVALID_ANNOTATIONS)
-    data_windows, annotation_windows = prep.extract_windows(WINDOW_SIZE, OVERLAP)
-    signal1_windows = data_windows[:, :, 0:1]
-    signal2_windows = data_windows[:, :, 1:2]
+    if DO_PREPROCESSING is True:
+        prep = Preprocessing(p_212.record, p_212.annotation, VALID_ANNOTATIONS, INVALID_ANNOTATIONS)
+        data_windows, annotation_windows = prep.extract_windows(WINDOW_SIZE, OVERLAP)
+        signal1_windows = data_windows[:, :, 0:1]
+        signal2_windows = data_windows[:, :, 1:2]
 
-    save = Save(212, signal1_windows, signal2_windows, annotation_windows)
-    load = Load(212)
-
-    save.save_data_json()
-    save.save_data_csv()
-    signal1_windows, signal2_windows, annotation_windows = load.load_data_json()
-    signal1_windows_c, signal2_windows_c, annotation_windows_c = load.load_data_csv()
+        save = Save(212, signal1_windows, signal2_windows, annotation_windows)
+        save.save_data_json()
+        save.save_data_csv()
+        
+    else:
+        load = Load(212)
+        signal1_windows, signal2_windows, annotation_windows = load.load_data_json()
+        signal1_windows_c, signal2_windows_c, annotation_windows_c = load.load_data_csv()
 
     print(signal1_windows.shape)
     print(signal2_windows.shape)
