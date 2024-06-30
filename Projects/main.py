218ccc47a9d0519a27ecd6006d286e0e0fcb785e
JustinLungu <iustinl113@gmail.com>
2024-06-30 15:01:36 +0200

    structured main

--- Projects/main.py
+++ Projects/main.py
@@ -15,29 +15,18 @@ PATIENT_NUMBER = 212
 WINDOW_SIZE = 200
 OVERLAP = 0.5  # 0.5 = 50%
 VALID_ANNOTATIONS = {"N", "R"}
+#convert labels (continue with 2,3,4 etc. depending on how many classes you have)
+LABEL_MAPPING = {"N": 0, "R": 1}
 NUM_CLASSES = len(VALID_ANNOTATIONS)
 INVALID_ANNOTATIONS = {"~", "+", "|"}
 
+DO_VISUALIZATION = False
 DO_PREPROCESSING = False
 DO_TRAINING = True
 
 SAMPLING_RATE = 360
 LOWCUT = 0.5
 HIGHCUT = 40.0
-
-# there are also some especially noisy sections, but they are long
-# if we removed them, we will lose data
-#######################################################################################
-"""
-it was recommended by the professor to not remove manually the artifacts unless we find
-a way to automate the artifact removal as we would need to keep these artifacts in the
-testing data if we are to deploy to real life this project.
-"""
-# 400000 - 406000
-# 578300 - 581200
-# ARTIFACTS = [[232000, 232600], [252250, 252700], [375600, 375750],
-#             [502300, 503000], [572500, 572750], [580800, 581200]]
-#######################################################################################
 ARTIFACTS = []
 
 TRAIN_RATIO = 0.7
@@ -59,7 +48,8 @@ def split_balance_norm(
     signal2_windows,
     annotation_windows,
 ):
-    # Split the data
+    
+
     prep.split_data(
         signal1_windows,
         signal2_windows,
@@ -69,33 +59,35 @@ def split_balance_norm(
         TEST_RATIO,
     )
 
-    # Plotting each signal individually from train, validation and test
+    #plot each signal individually from train, validation and test
+    if DO_VISUALIZATION is True:
+        patient.plot_1_signal(prep.signal1.train, prep.labels.train, title="Signal 1 Training Data", filename="s1_train.png") # noqa
+        patient.plot_1_signal(prep.signal1.val, prep.labels.val, title="Signal 1 Validation Data", filename="s1_val.png") # noqa
+        patient.plot_1_signal(prep.signal1.test, prep.labels.test, title="Signal 1 Test Data", filename="s1_test.png") # noqa
 
-    # patient.plot_1_signal(prep.signal1.train, prep.labels.train, title="Signal 1 Training Data", filename="s1_train.png") # noqa
-    # patient.plot_1_signal(prep.signal1.val, prep.labels.val, title="Signal 1 Validation Data", filename="s1_val.png") # noqa
-    # patient.plot_1_signal(prep.signal1.test, prep.labels.test, title="Signal 1 Test Data", filename="s1_test.png") # noqa
-
-    # patient.plot_1_signal(prep.signal2.train, prep.labels.train, title="Signal 2 Training Data", filename="s2_train.png") # noqa
-    # patient.plot_1_signal(prep.signal2.val, prep.labels.val, title="Signal 2 Validation Data", filename="s2_val.png") # noqa
-    # patient.plot_1_signal(prep.signal2.test, prep.labels.test, title="Signal 2 Test Data", filename="s2_test.png") # noqa
+        patient.plot_1_signal(prep.signal2.train, prep.labels.train, title="Signal 2 Training Data", filename="s2_train.png") # noqa
+        patient.plot_1_signal(prep.signal2.val, prep.labels.val, title="Signal 2 Validation Data", filename="s2_val.png") # noqa
+        patient.plot_1_signal(prep.signal2.test, prep.labels.test, title="Signal 2 Test Data", filename="s2_test.png") # noqa
 
     prep.balance_data()
-
     prep.normalize_minmax()
 
-    # patient.plot_1_signal(prep.signal2.train, prep.labels.train, title="Signal 1 Training Data Normalized", filename="s1_train_norm.png") # noqa
-    # patient.plot_1_signal(prep.signal2.val, prep.labels.val, title="Signal 1 Validation Data Normalized", filename="s1_val_norm.png") # noqa
-    # patient.plot_1_signal(prep.signal2.test, prep.labels.test, title="Signal 1 Test Data Normalized", filename="s1_test_norm.png") # noqa
+    if DO_VISUALIZATION is True:
+        patient.plot_1_signal(prep.signal2.train, prep.labels.train, title="Signal 1 Training Data Normalized", filename="s1_train_norm.png") # noqa
+        patient.plot_1_signal(prep.signal2.val, prep.labels.val, title="Signal 1 Validation Data Normalized", filename="s1_val_norm.png") # noqa
+        patient.plot_1_signal(prep.signal2.test, prep.labels.test, title="Signal 1 Test Data Normalized", filename="s1_test_norm.png") # noqa
 
-    # patient.plot_1_signal(prep.signal2.train, prep.labels.train, title="Signal 2 Training Data Normalized", filename="s2_train_norm.png") # noqa
-    # patient.plot_1_signal(prep.signal2.val, prep.labels.val, title="Signal 2 Validation Data Normalized", filename="s2_val_norm.png") # noqa
-    # patient.plot_1_signal(prep.signal2.test, prep.labels.test, title="Signal 2 Test Data Normalized", filename="s2_test_norm.png") # noqa
+        patient.plot_1_signal(prep.signal2.train, prep.labels.train, title="Signal 2 Training Data Normalized", filename="s2_train_norm.png") # noqa
+        patient.plot_1_signal(prep.signal2.val, prep.labels.val, title="Signal 2 Validation Data Normalized", filename="s2_val_norm.png") # noqa
+        patient.plot_1_signal(prep.signal2.test, prep.labels.test, title="Signal 2 Test Data Normalized", filename="s2_test_norm.png") # noqa
 
 
 if __name__ == "__main__":
-    patient = Visualization(PATIENT_NUMBER, 1)
-    # p_212.plot_annotation_sep_channels()
-    # p_212.plot_annotation_signals()
+    patient = Visualization(p_number = PATIENT_NUMBER, plot_artifacts = 1)
+    if DO_VISUALIZATION is True:
+        patient.plot_annotation_sep_channels()
+        patient.plot_annotation_signals()
+
     prep = Preprocessing(
         patient.record,
         patient.annotation,
@@ -103,8 +95,8 @@ if __name__ == "__main__":
         INVALID_ANNOTATIONS,
         ARTIFACTS,
     )
-
-    # Example usage
+    
+    ############################### PREPROCESSING ###############################################
     if DO_PREPROCESSING is True:
         # patient.plot_all()
         prep.butterworth(SAMPLING_RATE, LOWCUT, HIGHCUT)
@@ -150,23 +142,22 @@ if __name__ == "__main__":
         signal2_windows,
         annotation_windows,
     )
+    #################################################################################################
 
-    # Training the Model
+
+    ###################################### TRAINING ###################################################
     train_data = [prep.signal1.train, prep.signal2.train]
     val_data = [prep.signal1.val, prep.signal2.val]
     test_data = [prep.signal1.test, prep.signal2.test]
 
-    # Convert labels to binary (0 and 1)
-    label_mapping = {"N": 0, "R": 1}
-
-    train_labels = np.vectorize(label_mapping.get)(prep.labels.train)
-    val_labels = np.vectorize(label_mapping.get)(prep.labels.val)
-    test_labels = np.vectorize(label_mapping.get)(prep.labels.test)
+    train_labels = np.vectorize(LABEL_MAPPING.get)(prep.labels.train)
+    val_labels = np.vectorize(LABEL_MAPPING.get)(prep.labels.val)
+    test_labels = np.vectorize(LABEL_MAPPING.get)(prep.labels.test)
 
     sl_model = Model_Save_Load(PATIENT_NUMBER, MODEL_NAME)
 
     if DO_TRAINING:
-        # Initialize and train the model
+        # initialize model
         cnn_model = CNNModel(
             train_data[0].shape[1:],
             train_data[1].shape[1:],
@@ -201,18 +192,17 @@ if __name__ == "__main__":
         # load model
         evaluation = Evaluation(None)
         cnn_model = sl_model.load_model_h5()
+    #######################################################################################################
 
-    # Evaluate the model
+    ############################### EVALUATION ###########################################################
     test_loss, test_accuracy = cnn_model.evaluate(test_data, test_labels)
     print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy}")
 
-    # Make predictions
     test_predictions = cnn_model.predict(test_data)
-    #test_predictions = cnn_model.model.predict(test_data)
     test_predictions = np.round(test_predictions).astype(int)
 
-    # Calculate F1 Score
+    #F1 Score
     f1 = evaluation.calculate_f1_score(test_labels, test_predictions)
 
-    # Plot confusion matrix
+    #Confusion matrix
     evaluation.plot_confusion_matrix(test_labels, test_predictions, labels=["N", "R"])
